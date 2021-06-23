7ef759a212c5797b7ffae6e7e09f2a862c2d70cf
Vladimir <vladkodoktorov@gmail.com>
2021-06-23 13:54:42 +0100

    added random functions

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,15 +1,19 @@
 pragma solidity ^0.4.17;
 
-contract Lottery{
+contract Lottery {
     address public manager;
     address[] public players;
-    
+
     constructor() public {
         manager = msg.sender;
     }
-    
+
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
+    }
+
+    function random() private view returns (uint256) {
+        return uint256(keccak256(block.difficulty, now, players));
     }
 }
