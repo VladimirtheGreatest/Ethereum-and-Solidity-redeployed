0c7dd998e51f5b4677e3b65c71c275c94b54c386
Vladimir <vladkodoktorov@gmail.com>
2021-06-25 09:26:24 +0100

    pick random winner function

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,19 +1,23 @@
 pragma solidity ^0.4.17;
 
-contract Lottery {
+contract Lottery{
     address public manager;
     address[] public players;
-
+    
     constructor() public {
         manager = msg.sender;
     }
-
+    
     function enter() public payable {
         require(msg.value > .01 ether);
         players.push(msg.sender);
     }
 
-    function random() private view returns (uint256) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, players, now)));
+    function random() private view returns(uint) {
+    return uint(keccak256(abi.encodePacked(block.difficulty, players, now)));
+    }
+    function pickWinner() public {
+        uint index = random() % players.length;
+        players[index].transfer(address(this).balance); 
     }
 }
