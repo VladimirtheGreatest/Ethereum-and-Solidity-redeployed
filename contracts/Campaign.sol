6ca8380a2cdcb2b5542420caf014fc151573749d
Vladimir <vladkodoktorov@gmail.com>
2021-10-19 17:47:37 +0100

    refactoring adding factory contract

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,39 +1,55 @@
 pragma solidity ^0.4.17;
 
-contract Campaign{
-    struct Request{
+contract CampaignFactory {
+    address[] public deployedCampaigns;
+
+    function createCampaign(uint256 minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    function getDeployedCampaigns() public view returns (address[]) {
+        return deployedCampaigns;
+    }
+}
+
+contract Campaign {
+    struct Request {
         string description;
-        uint value;
+        uint256 value;
         address recipient;
         bool complete;
-        uint approvalCount; //if this is more than 50% of approvers then the Request will be successfull
+        uint256 approvalCount; //if this is more than 50% of approvers then the Request will be successfull
         mapping(address => bool) approvals; //approvals for particular requests
     }
-    
+
     Request[] public requests;
     address public manager;
-    uint public minimumContribution;
+    uint256 public minimumContribution;
     mapping(address => bool) public approvers; //list of people that contributed and can now approve requests
-    uint public approversCount;  //could be approvers.length
-    modifier restricted(){
+    uint256 public approversCount; //could be approvers.length
+    modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-    
-    constructor(uint minimum) public {
-        manager = msg.sender;
+
+    constructor(uint256 minimum, address creator) public {
+        manager = creator;
         minimumContribution = minimum;
     }
 
-    function contribute() public payable{
+    function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
         approversCount++;
-        
     }
-    function createRequest(string description, uint value, address recipient)
-    public restricted {
-         Request memory newRequest = Request({
+
+    function createRequest(
+        string description,
+        uint256 value,
+        address recipient
+    ) public restricted {
+        Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
@@ -42,21 +58,23 @@ contract Campaign{
         });
         requests.push(newRequest);
     }
-    function approveRequest(uint index) public {
+
+    function approveRequest(uint256 index) public {
         Request storage request = requests[index];
-        
+
         require(approvers[msg.sender]); //check if the approver contributed first
-        require(!request.approvals[msg.sender]); // check if the approver has already approved this request 
-        
+        require(!request.approvals[msg.sender]); // check if the approver has already approved this request
+
         request.approvals[msg.sender] = true; //all checks done approve the request
         request.approvalCount++;
     }
-    function finalizeRequest(uint index) public restricted{
+
+    function finalizeRequest(uint256 index) public restricted {
         Request storage request = requests[index];
         require(request.approvalCount > (approversCount / 2));
         require(!request.complete);
-        
-         request.recipient.transfer(request.value);
-         request.complete = true;
+
+        request.recipient.transfer(request.value);
+        request.complete = true;
     }
 }
