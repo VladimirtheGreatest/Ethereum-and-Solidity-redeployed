82625e3b455d844a47c2fd1f7752d3262febe7bb
Vladimir <vladkodoktorov@gmail.com>
2021-10-19 16:08:38 +0100

    contract changes

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,41 +1,39 @@
 pragma solidity ^0.4.17;
 
-contract Campaign {
-    struct Request {
+contract Campaign{
+    struct Request{
         string description;
-        uint256 value;
+        uint value;
         address recipient;
         bool complete;
-        uint256 approvalCount; //if this is more than 50% of approvers then the Request will be successfull
-        mapping(address => bool) approvals;
+        uint approvalCount; //if this is more than 50% of approvers then the Request will be successfull
+        mapping(address => bool) approvals; //approvals for particular requests
     }
-
+    
     Request[] public requests;
     address public manager;
-    uint256 public minimumContribution;
-    mapping(address => bool) public approvers;
-    modifier restricted() {
+    uint public minimumContribution;
+    mapping(address => bool) public approvers; //list of people that contributed and can now approve requests
+    uint public approversCount;  //could be approvers.length
+    modifier restricted(){
         require(msg.sender == manager);
         _;
     }
-
-    constructor(uint256 minimum) public {
+    
+    constructor(uint minimum) public {
         manager = msg.sender;
         minimumContribution = minimum;
     }
 
-    function contribute() public payable {
+    function contribute() public payable{
         require(msg.value > minimumContribution);
         approvers[msg.sender] = true;
+        approversCount++;
+        
     }
-
-    function createRequest(
-        string description,
-        uint256 value,
-        address recipient
-    ) public restricted {
-        // require(approvers[msg.sender]);
-        Request memory newRequest = Request({
+    function createRequest(string description, uint value, address recipient)
+    public restricted {
+         Request memory newRequest = Request({
             description: description,
             value: value,
             recipient: recipient,
@@ -44,14 +42,21 @@ contract Campaign {
         });
         requests.push(newRequest);
     }
-
-    function approveRequest(uint256 index) public {
+    function approveRequest(uint index) public {
         Request storage request = requests[index];
-
-        require(approvers[msg.sender]);
-        require(!request.approvals[msg.sender]);
-
-        request.approvals[msg.sender] = true;
+        
+        require(approvers[msg.sender]); //check if the approver contributed first
+        require(!request.approvals[msg.sender]); // check if the approver has already approved this request 
+        
+        request.approvals[msg.sender] = true; //all checks done approve the request
         request.approvalCount++;
+    }
+    function finalizeRequest(uint index) public restricted{
+        Request storage request = requests[index];
+        require(request.approvalCount > (approversCount / 2));
+        require(!request.complete);
+        
+         request.recipient.transfer(request.value);
+         request.complete = true;
     }
 }
--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -1,151 +1,137 @@
 pragma solidity ^0.4.17;
 
-contract Casino {
-    struct Deposit {
-        uint256 value;
+
+contract Casino{
+    struct Deposit{
+        uint value;
         address gambler;
     }
-
+    
     Deposit[] public deposits;
     address public house;
-    uint256 public minimumContribution;
+    uint public minimumContribution;
     address[] public players;
-    uint256 public progressiveJackpot;
-    bool locked = false;
-
+    uint public progressiveJackpot;
+    bool locked = false;  //maybe add some counter for players, 
+    
     //house cannot make deposits or play the game that would not be fair
-    modifier restricted() {
-        require(msg.sender != house);
+    modifier restricted(){
+        require(msg.sender != house); 
         _;
     }
-
-    constructor(uint256 minimum, uint256 progressiveJackpotGuarantee) public {
+    
+    constructor(uint minimum, uint progressiveJackpotGuarantee) public {
         house = msg.sender;
         minimumContribution = minimum;
         progressiveJackpot = progressiveJackpotGuarantee;
     }
 
-    function deposit() public payable restricted {
+    function deposit() public restricted payable{
         require(msg.value > minimumContribution);
         players.push(msg.sender); //with every deposit you increase your chance to win the jackpot
         progressiveJackpot += msg.value; //increase the progressive jackpot with every deposit
-
-        if (checkDeposits(deposits, msg.sender)) {
-            //check if we have a deposit for this player
-
-            for (uint256 i = 0; i < deposits.length; i++) {
-                if (deposits[i].gambler == msg.sender) {
-                    deposits[i].value += msg.value; //if the player deposited before add additional deposit to his account
-                }
+        
+        if(checkDeposits(deposits,msg.sender)){  //check if we have a deposit for this player
+        
+        for (uint i = 0; i < deposits.length; i++) { 
+        if (deposits[i].gambler == msg.sender) { 
+            deposits[i].value += msg.value;       //if the player deposited before add additional deposit to his account
+          } 
+         }
+       } else  { //we have not found deposit so create a new one
+             Deposit memory newDeposit =  Deposit({  //try mapping address integer
+            value: msg.value,
+            gambler: msg.sender
+        });
+        deposits.push(newDeposit);
+       }
+    }
+    
+        function play() public restricted returns(string memory response) {
+        if(checkBalance() < minimumContribution){
+                revert("Please deposit first");
+        }
+        for (uint i = 0; i < deposits.length; i++) { 
+        if (deposits[i].gambler == msg.sender && deposits[i].value >= minimumContribution) { 
+            deposits[i].value -= minimumContribution;       //if the player deposited before deduct the playing fee from his account
+            //logic for winning and loosing here, also to hit the jackpot
+            if(players.length > 10){
+             uint randomNumber = random() % players.length;
+             if(randomNumber > 10){ //need to come up with a better logic
+                 pickWinner();
+             }
             }
-        } else {
-            //we have not found deposit so create a new one
-            Deposit memory newDeposit = Deposit({
-                value: msg.value,
-                gambler: msg.sender
-            });
-            deposits.push(newDeposit);
-        }
+            return "success";
+          } 
+         }
     }
-
-    function play() public restricted returns (string memory response) {
-        if (checkBalance() < minimumContribution) {
-            revert("Please deposit first");
-        }
-        for (uint256 i = 0; i < deposits.length; i++) {
-            if (
-                deposits[i].gambler == msg.sender &&
-                deposits[i].value >= minimumContribution
-            ) {
-                deposits[i].value -= minimumContribution; //if the player deposited before deduct the playing fee from his account
-                //logic for winning and loosing here, also to hit the jackpot
-                if (players.length > 10) {
-                    uint256 randomNumber = random() % players.length;
-                    if (randomNumber > 10) {
-                        //need to come up with a better logic
-                        pickWinner();
-                    }
-                }
-                return "success";
-            }
-        }
+    
+       function random() private view returns(uint) {
+    return uint(keccak256(abi.encodePacked(block.difficulty, players, now)));
     }
-
-    function random() private view returns (uint256) {
-        return
-            uint256(
-                keccak256(abi.encodePacked(block.difficulty, players, now))
-            );
-    }
-
+    
     function pickWinner() private {
-        uint256 index = random() % players.length;
-        require(!locked, "Reentrant call detected!"); //Use a Reentrancy Guard
-        locked = true;
-        bool successWinnerTransfer = players[index].call.value(
-            (address(this).balance / 100) * 90
-        )(""); //90% from progressive jackpot goes to the winner
+        uint index = random() % players.length;
+        require(!locked, "Reentrant call detected!");  //Use a Reentrancy Guard
+       locked = true;
+       (bool successWinnerTransfer) = players[index].call.value((address(this).balance / 100) * 90)(""); //90% from progressive jackpot goes to the winner
         require(successWinnerTransfer, "Transfer to the winner failed.");
-        bool successHouseTransfer = house.call.value(address(this).balance)(""); // 10% rake fee for the house
+       // players[index].transfer(address(this).balance * 0.9); 
+       // house.transfer(address(this).balance); 
+        (bool successHouseTransfer) = house.call.value(address(this).balance)(""); // 10% rake fee for the house
         require(successHouseTransfer, "Transfer to house failed.");
-        players = new address[](0); //reset the lottery
-        progressiveJackpot = 0;
+        players = new address[](0);   //reset the lottery
+        progressiveJackpot = 0;  
         locked = false;
     }
+    
 
-    function checkBalance() public view restricted returns (uint256 balance) {
-        require(
+    function checkBalance() public restricted view returns(uint balance){
+           require(
             checkDeposits(deposits, msg.sender),
             "Could not find deposit this player."
         );
-        for (uint256 i = 0; i < deposits.length; i++) {
-            if (deposits[i].gambler == msg.sender) {
-                return deposits[i].value;
-            }
-        }
+        for (uint i = 0; i < deposits.length; i++) { 
+        if (deposits[i].gambler == msg.sender) { 
+        return deposits[i].value;
+        } 
+      }
     }
-
-    function checkProgressiveJackpot()
-        public
-        view
-        restricted
-        returns (uint256 jackpot)
-    {
-        return progressiveJackpot;
+    
+    function checkProgressiveJackpot() public restricted view returns(uint jackpot){
+           return progressiveJackpot;
     }
-
-    function checkDeposits(Deposit[] memory array, address sender)
-        private
-        pure
-        returns (bool isTrue)
-    {
-        for (uint256 i = 0; i < array.length; i++) {
-            if (array[i].gambler == sender) {
-                return true;
-            }
-        }
+    
+        function checkDeposits(Deposit[] memory array, address sender)private pure returns(bool isTrue) {
+     for (uint i = 0; i < array.length; i++) { 
+        if (array[i].gambler == sender) { 
+        return true;
+        } 
+      }
     }
-
+    
+    
     //converts uint to string
-    function uint2str(uint256 _i) internal pure returns (string memory str) {
-        if (_i == 0) {
-            return "0";
-        }
-        uint256 j = _i;
-        uint256 length;
-        while (j != 0) {
-            length++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(length);
-        uint256 k = length;
-        j = _i;
-        while (j != 0) {
-            bstr[--k] = bytes1(uint8(48 + (j % 10)));
-            j /= 10;
-        }
-        str = string(
-            abi.encodePacked("Please make a deposit of at least ", bstr)
-        ); //string concatenation is not supported yet so this hack
-    }
+     function uint2str (uint256 _i) internal pure returns (string memory str) {
+      if (_i == 0)
+      {
+        return "0";
+      }
+      uint256 j = _i;
+      uint256 length;
+      while (j != 0)
+      {
+        length++;
+        j /= 10;
+      }
+      bytes memory bstr = new bytes(length);
+      uint256 k = length;
+      j = _i;
+      while (j != 0)
+      {
+        bstr[--k] = bytes1(uint8(48 + j % 10));
+        j /= 10;
+      }
+      str =  string(abi.encodePacked("Please make a deposit of at least ", bstr));  //string concatenation is not supported yet so this hack
+     }
 }
